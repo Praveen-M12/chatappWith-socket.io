8584feac3fbbce1b4f00b5b2614b62dd230b2cce
DESKTOP-GHR1TMQ\prave <praveenm@bicsinfotech.com>
2024-03-21 15:34:01 +0530

    add comment

--- src/client.js
+++ src/client.js
@@ -1,55 +1,3 @@
-// const socket = io()
-// let name;
-// let textarea = document.querySelector('#textarea')
-// let messageArea = document.querySelector('.message__area')
-// do {
-//     name = prompt('Please enter your name: ')
-// } while(!name)
-
-// textarea.addEventListener('keyup', (e) => {
-//     if(e.key === 'Enter') {
-//         sendMessage(e.target.value)
-//     }
-// })
-
-// function sendMessage(message) {
-//     let msg = {
-//         user: name,
-//         message: message.trim()
-//     }
-//     // Append 
-//     appendMessage(msg, 'outgoing')
-//     textarea.value = ''
-//     scrollToBottom()
-
-//     // Send to server 
-//     socket.emit('message', msg)
-
-// }
-
-// function appendMessage(msg, type) {
-//     let mainDiv = document.createElement('div')
-//     let className = type
-//     mainDiv.classList.add(className, 'message')
-
-//     let markup = `
-//         <h4>${msg.user}</h4>
-//         <p>${msg.message}</p>
-//     `
-//     mainDiv.innerHTML = markup
-//     messageArea.appendChild(mainDiv)
-// }
-
-// // Recieve messages 
-// socket.on('message', (msg) => {
-//     appendMessage(msg, 'incoming')
-//     scrollToBottom()
-// })
-
-// function scrollToBottom() {
-//     messageArea.scrollTop = messageArea.scrollHeight
-// }
-
 const socket = io();
 let name;
 let textarea = document.querySelector('#textarea');
@@ -67,6 +15,7 @@ do {
 // });
 
 textarea.addEventListener('keyup', (e) => {
+    //not send mesage if we not add the data
     if (e.key === 'Enter' && textarea.value.trim() !== '') { 
         sendMessage(textarea.value);
     }
